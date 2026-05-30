52b50942a6da990901e1e743b2d14cbd39023809
Atstincer <atstincer87@gmail.com>
2026-05-30 11:59:30 +0200

    adding redis persistent data

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -23,7 +23,7 @@ router.get('/statistics', async (req, res) => {
   console.log('statistics endpoit erreicht')
   const added_todos = await redis.get('todoCount')
   console.log('added_todos',added_todos)
-  res.send({"added_todos": added_todos.length === 0 ? '0' : added_todos});
+  res.send({"added_todos": added_todos === null || added_todos.length === 0 ? '0' : added_todos});
 });
 
 module.exports = router;
